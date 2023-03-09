3fe7624e25e43eb296f7efd7b9bd9153aeda7d81
Sergio Gonzalez <89554725+sergiogonzalezyes@users.noreply.github.com>
2023-03-09 15:18:34 -0600

    Revert "fixed deploy.sh"
    This reverts commit e1fc77111ecfb212d9bebcd2ea7925355074dbff.

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,14 @@
-
-
-set -e
-
 npm run build
+
+LOGSTRING=$(git log)
+COMMIT=$(echo $LOGSTRING | awk '{print $2}')
 
 cd dist
 
 git init
 git add -A
-git commit -m "deploy"
+git commit -m "deploy (commit: $COMMIT)"
 
-git push -f https://github.com/sergiogonzalezyes/Portfolio.git main:gh-pages
+git push -f https://github.com/sergiogonzalezyes/SuperheroVillain-API-App.git main:gh-pages
 
-cd -
+cd ..
